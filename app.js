1630d87d580e307c7281e50b6b1941a84d9b1a7e
raziyeh <rsoradimoghaddam@gmail.com>
2023-04-05 03:33:11 +0430

    Added nodemon package for development

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require('fs')
+const fs = require("fs")
 
-const path = require('path')
+const path = require("path")
 
 const express = require("express")
 
@@ -22,21 +22,21 @@ app.get("/", function (req, res) {
 
 app.post("/store-user", function (req, res) {
 	const userName = req.body.username
-	const filePath = path.join(__dirname, 'data', 'users.json')
+	const filePath = path.join(__dirname, "data", "users.json")
 	const usersData = JSON.parse(fs.readFileSync(filePath))
 	usersData.push(userName)
 	fs.writeFileSync(filePath, JSON.stringify(usersData))
 	res.send("<h1>Username stored!</h1>")
 })
 
-app.get("/users", function(req, res) {
-	const filePath = path.join(__dirname, 'data', 'users.json')
+app.get("/users", function (req, res) {
+	const filePath = path.join(__dirname, "data", "users.json")
 	const usersData = JSON.parse(fs.readFileSync(filePath))
-	let usersHTML = ''
+	let usersHTML = ""
 	for (user of usersData) {
-		usersHTML += `<li>${user}</li>`
+		usersHTML += `<li>Welcome ${user}</li>`
 	}
-	res.send(`<ul>${usersHTML}</ul>`);
+	res.send(`<ul>${usersHTML}</ul>`)
 })
 
 app.listen(3000)
