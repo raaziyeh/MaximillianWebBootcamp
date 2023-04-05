4feb2f59099bc0b3b44d3db01888c13156c819b4
raziyeh <rsoradimoghaddam@gmail.com>
2023-04-06 03:40:41 +0430

    Outputed repeated content with EJS and loops

--- app.js
+++ app.js
@@ -5,44 +5,47 @@ const express = require("express")
 
 const app = express()
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
 
-app.use(express.static('public'))
+app.use(express.static("public"))
 
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.get("/", function (req, res) {
-	res.render('index')
+	res.render("index")
 })
 
 app.get("/about", function (req, res) {
-	res.render('about')
+	res.render("about")
 })
 
 app.get("/confirm", function (req, res) {
-	res.render('confirm')
+	res.render("confirm")
 })
 
 app.get("/recommend", function (req, res) {
-	res.render('recommend')
+	res.render("recommend")
 })
 
-app.post("/recommend", function(req, res) {
+app.post("/recommend", function (req, res) {
 	const restaurant = req.body
-	const filePath = path.join(__dirname, 'data', 'restaurants.json')
+	const filePath = path.join(__dirname, "data", "restaurants.json")
 	const fileData = fs.readFileSync(filePath)
 	const restaurantsData = JSON.parse(fileData)
 	restaurantsData.push(restaurant)
 	fs.writeFileSync(filePath, JSON.stringify(restaurantsData))
-	res.redirect('/confirm')
+	res.redirect("/confirm")
 })
 
 app.get("/restaurants", function (req, res) {
 	const filePath = path.join(__dirname, "data", "restaurants.json")
 	const fileData = fs.readFileSync(filePath)
 	const restaurantsData = JSON.parse(fileData)
-	res.render("restaurants", { numberOfRestaurants: restaurantsData.length })
+	res.render("restaurants", {
+		numberOfRestaurants: restaurantsData.length,
+		restaurants: restaurantsData,
+	})
 })
 
 app.listen(3000)
