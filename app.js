80bb22c1b2c9c79c1c54026990425e8c25b4a412
raziyeh <rsoradimoghaddam@gmail.com>
2023-04-06 03:00:07 +0430

    Parsed form data and stored them

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const path = require("path")
+const fs = require("fs")
 
 const express = require("express")
 
 const app = express()
 
 app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: false}))
 
 app.get("/", function (req, res) {
 	const htmlFilePath = path.join(__dirname, "views", "index.html")
@@ -24,6 +27,16 @@ app.get("/confirm", function (req, res) {
 app.get("/recommend", function (req, res) {
 	const htmlFilePath = path.join(__dirname, "views", "recommend.html")
 	res.sendFile(htmlFilePath)
+})
+
+app.post("/recommend", function(req, res) {
+	const restaurant = req.body
+	const filePath = path.join(__dirname, 'data', 'restaurants.json')
+	const fileData = fs.readFileSync(filePath)
+	const restaurantsData = JSON.parse(fileData)
+	restaurantsData.push(restaurant)
+	fs.writeFileSync(filePath, JSON.stringify(restaurantsData))
+	res.redirect('/confirm')
 })
 
 app.get("/restaurants", function (req, res) {
