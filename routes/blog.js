2db5ac1c76005c8f43c8d343948a1cfd231667ec
raziyeh <rsoradimoghaddam@gmail.com>
2023-04-17 09:51:24 +0430

    Fetched authors from database and render them

---
+++ routes/blog.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const db = require('../data/database')
+
+const router = express.Router()
+
+router.get("/", function(req, res) {
+    res.redirect("/posts")
+})
+
+router.get("/posts", function(req, res) {
+    res.render("posts-list", {posts: []})
+})
+
+router.get("/new-post", async function (req, res) {
+    const authors = await db.getDb().collection('authors').find().toArray()
+  	res.render("create-post", {authors})
+})
+
+
+
+module.exports = router
