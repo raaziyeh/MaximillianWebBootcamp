5dbcf1d0fc9d9c13d8b94def7e939c217ff6bcbf
raziyeh <rsoradimoghaddam@gmail.com>
2023-04-18 11:20:04 +0430

    Added Update and Delete features to the posts

--- routes/blog.js
+++ routes/blog.js
@@ -41,6 +41,66 @@ router.post("/posts", async function (req, res) {
 	res.redirect("/posts")
 })
 
+router.get("/posts/:id", async function (req, res) {
+	const postId = req.params.id
+	const post = await db
+		.getDb()
+		.collection("posts")
+		.findOne({ _id: new ObjectId(postId) }, { summary: 0 })
+
+	if (!post) {
+		return res.status(404).render("404")
+	}
+
+	post.humanReadableDate = post.date.toLocaleDateString("en-US", {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	})
+	post.date = post.date.toISOString()
+
+	res.render("post-detail", { post })
+})
+
+router.get("/posts/:id/edit", async function (req, res) {
+	const postId = req.params.id
+	const post = await db
+		.getDb()
+		.collection("posts")
+		.findOne({ _id: new ObjectId(postId) }, { title: 1, summary: 1, body: 1 })
+
+	if (!post) {
+		res.status(404).render("404")
+	}
+
+	res.render("update-post", { post })
+})
+
+router.post("/posts/:id/edit", async function (req, res) {
+	const postId = req.params.id
+	const newPost = {
+		title: req.body.title,
+		summary: req.body.summary,
+		body: req.body.content,
+	}
+
+	await db
+		.getDb()
+		.collection("posts")
+		.updateOne({ _id: new ObjectId(postId) }, { $set: newPost })
+
+	res.redirect("/posts")
+})
+
+router.post("/posts/:id/delete", async function (req, res) {
+	await db
+		.getDb()
+		.collection("posts")
+		.deleteOne({ _id: new ObjectId(req.params.id) })
+	res.redirect("/posts")
+})
+
 router.get("/new-post", async function (req, res) {
 	const authors = await db.getDb().collection("authors").find().toArray()
 
