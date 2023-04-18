20029fe88e356fbb1f7d517aefc4f2d1e55b183a
raziyeh <rsoradimoghaddam@gmail.com>
2023-04-18 08:30:32 +0430

    Handled post request and Inserted into Mongodb

--- routes/blog.js
+++ routes/blog.js
@@ -1,22 +1,45 @@
-const express = require('express')
+const express = require("express")
 
-const db = require('../data/database')
+const db = require("../data/database")
+const { ObjectId } = require("mongodb")
 
 const router = express.Router()
 
-router.get("/", function(req, res) {
-    res.redirect("/posts")
+router.get("/", function (req, res) {
+	res.redirect("/posts")
 })
 
-router.get("/posts", function(req, res) {
-    res.render("posts-list", {posts: []})
+router.get("/posts", function (req, res) {
+	res.render("posts-list", { posts: [] })
+})
+
+router.post("/posts", async function (req, res) {
+	const authorId = new ObjectId(req.body.author)
+	const author = await db
+		.getDb()
+		.collection("authors")
+		.findOne({ _id: authorId })
+
+	const newPost = {
+		title: req.body.title,
+		summary: req.body.summary,
+		body: req.body.content,
+		date: new Date(),
+		author: {
+			id: authorId,
+			name: author.name,
+			email: author.email,
+		},
+	}
+
+	const result = await db.getDb().collection("posts").insertOne(newPost)
+	res.redirect("/posts")
 })
 
 router.get("/new-post", async function (req, res) {
-    const authors = await db.getDb().collection('authors').find().toArray()
-  	res.render("create-post", {authors})
+	const authors = await db.getDb().collection("authors").find().toArray()
+
+	res.render("create-post", { authors })
 })
-
-
 
 module.exports = router
