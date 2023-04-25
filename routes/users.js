4b0e35e02d8787b856c7ca8a22b6973adfd4f3ec
raziyeh <rsoradimoghaddam@gmail.com>
2023-04-25 13:30:36 +0430

    Added multer package and recieved and stored image file

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,17 @@
 const express = require("express")
+const multer = require("multer")
 
 const router = express.Router()
+
+const storageConfig = multer.diskStorage({
+	destination: function(req, file, cb) {
+		cb(null, "images")
+	},
+	filename: function(req, file, cb) {
+		cb(null, Date.now() + "-" + file.originalname)
+	}
+})
+const upload = multer({storage: storageConfig})
 
 router.get("/", function (req, res) {
 	res.render("profiles")
@@ -8,6 +19,16 @@ router.get("/", function (req, res) {
 
 router.get("/new-user", function (req, res) {
 	res.render("new-user")
+})
+
+router.post("/profiles", upload.single("image") ,function(req, res) {
+	const uploadedImageFile = req.file
+	const bodyData = req.body
+
+	console.log(uploadedImageFile)
+	console.log(bodyData)
+	
+	res.redirect("/")
 })
 
 module.exports = router
