2ba922db58bbcc60329ffe5e9f57b96520d4ffee
raziyeh <rsoradimoghaddam@gmail.com>
2023-04-26 18:13:50 +0430

    Served the uploaded files

--- routes/users.js
+++ routes/users.js
@@ -6,30 +6,32 @@ const db = require("../data/database")
 const router = express.Router()
 
 const storageConfig = multer.diskStorage({
-	destination: function(req, file, cb) {
+	destination: function (req, file, cb) {
 		cb(null, "images")
 	},
-	filename: function(req, file, cb) {
+	filename: function (req, file, cb) {
 		cb(null, Date.now() + "-" + file.originalname)
-	}
+	},
 })
-const upload = multer({storage: storageConfig})
+const upload = multer({ storage: storageConfig })
 
-router.get("/", function (req, res) {
-	res.render("profiles")
+router.get("/", async function (req, res) {
+	const users = await db.getDb().collection("users").find().toArray()
+	console.log(users)
+	res.render("profiles", { users })
 })
 
 router.get("/new-user", function (req, res) {
 	res.render("new-user")
 })
 
-router.post("/profiles", upload.single("image") ,async function(req, res) {
+router.post("/profiles", upload.single("image"), async function (req, res) {
 	const uploadedImageFile = req.file
 	const bodyData = req.body
 
-	await db.getDb().collection('users').insertOne({
-		username: req.body.username,
-		imagePath: req.file.path
+	await db.getDb().collection("users").insertOne({
+		username: bodyData.username,
+		imagePath: uploadedImageFile.path,
 	})
 
 	res.redirect("/")
